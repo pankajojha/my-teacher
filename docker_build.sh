d0688a37729020fc1133004d22633382231a28a5
pojha <pankaj.ojha@pearson.com>
2018-05-03 16:12:16 -0500

    updated with docker

---
+++ docker_build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+eval $(docker-machine env -u)
+docker build -t my-teacher .
+
+#if in background
+#docker run -d  -v ${PWD}/client:/usr/src/app -v /usr/src/app/node_modules -p 4200:4200 --name my-teacher-container my-teacher  
+
+docker run -it -v ${PWD}/client:/usr/src/app   -v /usr/src/app/node_modules -p 4200:4200 --rm my-teacher
