1f548ec8db1048a660b3760bc5b80787d557287d
pojha <pankaj.ojha@pearson.com>
2018-05-05 19:41:35 -0500

    docker and angular6 update

--- docker_build.sh
+++ docker_build.sh
@@ -4,5 +4,8 @@ docker build -t my-teacher .
 
 #if in background
 #docker run -d  -v ${PWD}/client:/usr/src/app -v /usr/src/app/node_modules -p 4200:4200 --name my-teacher-container my-teacher  
-
 docker run -it -v ${PWD}/client:/usr/src/app   -v /usr/src/app/node_modules -p 4200:4200 --rm my-teacher
+
+#docker run -it -v ${PWD}/client:/usr/src/app  -p 4200:4200 --rm my-teacher
+
+
